aaea9e74d0085f6842774c95908c23ee97c242ee
SahTitus <89194958+SahTitus@users.noreply.github.com>
2022-09-11 16:05:41 +0000

    commit

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "https://upay-engine.vercel.app" });
-// https://upay-engine.vercel.app
+const API = axios.create({ baseURL: "https://upay-back.vercel.app/ });
+// https://upay-back.vercel.app
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("profile")) {
